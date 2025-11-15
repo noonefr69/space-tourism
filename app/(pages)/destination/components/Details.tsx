1cd4ef7d55b9bec58490020ad0a7f49092014546
Noone <kasraam385@gmail.com>
2025-11-16 00:22:38 +0330

    animate the destination part

--- app/(pages)/destination/components/Details.tsx
+++ app/(pages)/destination/components/Details.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 
 type D = {
   name: string;
@@ -10,14 +11,30 @@ type D = {
 export default function Details({ destination }: { destination: D }) {
   return (
     <div className="row-span-2">
-      <h1 className="lg:text-8xl text-7xl text-center lg:text-start mb-7 font-serif text-white">
+      <motion.h1
+        layout
+        className="lg:text-8xl text-7xl text-center lg:text-start mb-7 font-serif text-white"
+      >
         {destination.name}
-      </h1>
-      <p className="lg:max-w-2/3 text-gray-400 text-center lg:text-start lg:text-lg font-medium">
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.05 }}
+        className="lg:max-w-2/3 text-gray-400 text-center lg:text-start lg:text-lg font-medium"
+      >
         {destination.description}
-      </p>
+      </motion.p>
+
       <div className="h-px w-full lg:max-w-2/3 rounded-full bg-[#ffffff1f] my-7" />
-      <div className="flex gap-5 pb-10 lg:gap-12 flex-col lg:flex-row items-center">
+
+      <motion.div
+        initial={{ opacity: 0, y: 6 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.08 }}
+        className="flex gap-5 pb-10 lg:gap-12 flex-col lg:flex-row items-center"
+      >
         <div>
           <h3 className="uppercase text-center lg:text-start text-gray-400 font-medium">
             Avg. distance
@@ -34,7 +51,7 @@ export default function Details({ destination }: { destination: D }) {
             {destination.travel}
           </h1>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- app/(pages)/destination/components/Tabs.tsx
+++ app/(pages)/destination/components/Tabs.tsx
@@ -13,11 +13,14 @@ export default function Tabs({
 }) {
   return (
     <>
-      {items.map((it) => (
+      {items.map((it, i) => (
         <button
           key={it.name}
+          style={{
+            animationDuration: `${0.1 + i * 0.1}s`,
+          }}
           onClick={() => onSelect(it.name)}
-          className={`text-xl text-[#ffffff74] pb-1 duration-200 border-b-2 border-transparent font-medium cursor-pointer ${
+          className={`text-xl animate-faceIn text-[#ffffff74] pb-1 duration-200 border-b-2 border-transparent font-medium cursor-pointer ${
             selected === it.name
               ? "text-white border-b-white"
               : "hover:border-b-white"
